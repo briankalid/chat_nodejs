3e6203e37912c2a23b2bb56d42bb5726789cca7c
briankalid <briankalid2000@gmail.com>
2021-02-18 19:00:49 -0600

    working without db and not good look

--- server.js
+++ server.js
@@ -2,19 +2,45 @@
 const http = require('http')
 const express = require('express')
 const debug = require('debug')('chat')
+const socketio = require('socket.io')
 
 const path = require('path')
 
 const port = 3000
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname,'public')))
+
+var messages = []; 
+// socket.io
+
+io.on('connect',socket => {
+    debug(`conncected ${socket.id}`)
+
+
+
+    socket.emit('messages', messages); 
+    socket.on('new-message', function(data) { 
+        messages.push(data); 
+        io.sockets.emit('messages', messages); 
+    }); 
+
+
+
+})
+
+
+    
+
+//
+
 
 
 //errores
 function handleFatalError(err){
-    console.error('Fatal error ${err.message}')
+    console.error(`Fatal error ${err.message}`)
     console.error(err.stack)
     process.exit(1)
 }
@@ -22,9 +48,10 @@ function handleFatalError(err){
 process.on('uncaughtException', handleFatalError)
 process.on('unhandleRejection', handleFatalError)
 
-
-
 //
+
+
+
 server.listen(port,() =>{
     console.log('listening on port 3000')
 })
